1c2018c7e020548cee021d92506034b27b9934ab
KoKimss <seungsu8848@ajou.ac.kr>
2023-04-01 13:43:45 +0900

    Feat : Splash Screen, Main Screen , Select Screen with anim,sound

---
+++ app/src/main/java/com/example/runninggame/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.runninggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+
+public class SplashActivity extends AppCompatActivity {
+    Animation anim;
+    LinearLayout linearLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        linearLayout=(LinearLayout)findViewById(R.id.activity_splash); // Declare an imageView to show the animation.
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein); // Create the animation.
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                // HomeActivity.class is the activity to go after showing the splash screen.
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        linearLayout.startAnimation(anim);
+    }
+}
